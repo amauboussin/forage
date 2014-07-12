ce91aa40f4b96557296f227357ebb497a25353b9
Andrew Mauboussin <ammaub@college.harvard.edu>
2014-07-12 03:37:24 -0700

    added yelp grid scraper

--- api/views.py
+++ api/views.py
@@ -9,14 +9,29 @@ from constants import *
 import json
 
 
-# tl_lat_orig = 37.433711
-# tl_long_orig = -122.110664
-# br_lat = tl_lat - 0.001
-# br_long = tl_long - 0.001
-# tl_lat_final = 37.357442
-# tl_long_final = -122.058908
-# DIFF_LAT = -0.076269
-# DIFF_LONG = 0.051756
+def scrape_grid_page(request):
+    scrape_grid()
+
+    return render_to_response('test.html', {'message' : 'finished scraping grid'}, context_instance=RequestContext(request))
+
+
+
+def scrape_grid():
+    ywsid = 'nc5nvTckUyLncvvm9Qd8ew'
+
+    tl_lat = 37.433711
+    tl_long = -122.110664
+    br_lat = tl_lat - 0.001
+    br_long = tl_long - 0.001
+
+    #37.357442
+    while tl_lat >= 37.357442:
+        req = 'http://api.yelp.com/business_review_search?term=food&tl_lat=%s&tl_long=%s&br_lat=%s&br_long=%s&limit=20&ywsid=%s' % (tl_lat, tl_long, br_lat, br_long, ywsid)
+        scrape_yelp_data(req)
+        tl_lat -= .001
+        tl_long += .001
+        br_lat = tl_lat - 0.001
+        br_long = tl_long - 0.001
 
 def get_loc(address):
     address = address.replace(' ', '+')
@@ -40,17 +55,13 @@ def get_genre(b, food_cats):
     return None
 
 
-def scrape(request):
-    lat = 37.423418
-    long = -122.071638
-    ywsid = 'nc5nvTckUyLncvvm9Qd8ew'
-    yelp_req = 'http://api.yelp.com/business_review_search?term=food&lat=%s&long=%s&radius=10&limit=20&ywsid=%s' % (lat, long, ywsid)
-    jsonurl = urlopen(yelp_req)
+def scrape_yelp_data(req):
+
+    jsonurl = urlopen(req)
     data = json.loads(jsonurl.read())
+    print data
 
     food_cats = get_food_cats()
-
-    print data
 
     for b in data['businesses']:
         name = b['name']
@@ -60,7 +71,6 @@ def scrape(request):
         #skip if no address or if the address already exists
         if len(b['address1']) == 0 : continue
         if Yelp.objects.filter(address= address).exists(): continue
-
 
         genre = get_genre(b, food_cats)
         if genre is None: continue
@@ -72,6 +82,15 @@ def scrape(request):
                    num_ratings = num_ratings,genre = genre, hours = '', latitude = lat, longitude = long)
         print name, genre
         r.save()
+
+def scrape(request):
+    lat = 37.423418
+    long = -122.071638
+
+    ywsid = 'nc5nvTckUyLncvvm9Qd8ew'
+    yelp_req = 'http://api.yelp.com/business_review_search?term=food&lat=%s&long=%s&radius=10&limit=20&ywsid=%s' % (lat, long, ywsid)
+    scrape_yelp_data(yelp_req)
+
     text = 'scraped'
 
     return render_to_response('test.html', {'message' : text}, context_instance=RequestContext(request))
--- forage/urls.py
+++ forage/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     #url()
+    url(r'scrapegrid',views.scrape_grid_page ),
     url(r'scrape',views.scrape ),
     url(r'locate', views.locate )
 
