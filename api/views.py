551fe4720292837037c1502da7020475703b88f5
John Conley <JCC282@gmail.com>
2014-07-11 21:04:37 -0700

    Find closest restaurants

--- api/views.py
+++ api/views.py
@@ -5,3 +5,17 @@ from django.template.context import RequestContext
 
 def test(request):
     return render_to_response('test.html', {}, context_instance=RequestContext(request))
+
+# returns the ten closest restaurants to (latitude, longitude)
+def get_restaurants(restaurants, latitude, longitude):
+    restaurants = Restaurant.objects.all()
+
+    def calculate_distance(restaurant):
+        rst_lat = restaurant.latitude
+        rst_long = restaurant.longitude
+        math.hypot(latitude - rst_lat, longitude - rst_long) # pythagorean theorem
+        return [distance, restaurant]
+
+    restaurantsWithDistance = map(calculate_distance, restaurants)
+    restaurantsWithDistance = sorted(restaurantsWithDistance, key = lambda x : x[0])
+    return restaurantsWithDistance[:10]
